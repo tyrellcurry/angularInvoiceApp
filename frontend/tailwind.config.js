9b06e3c823c7fd14ad6aec277ddbe225d9b9c0e9
Tyrell Curry <tcurry@mergeworld.com>
2024-01-15 16:39:26 -0800

    Updated tailwind colors, working on nav

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,7 +4,17 @@ module.exports = {
     "./src/**/*.{html,ts}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'faded-white': '#F8F8FB',
+        'dark-purple': '#141625',
+        'mid-dark-purple': '#373B53',
+        'mid-purple': '#494E6E',
+        'faded-purple': '#858BB2',
+        'faded-hover-purple': '#DFE3FA',
+
+      },
+    },
   },
   plugins: [],
 }
